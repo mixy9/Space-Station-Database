1bf8805a10d2e9411ef556a680a9b6c9bde0d011
Mihaela Trempetić <32740446+mixy9@users.noreply.github.com>
2019-03-03 16:45:47 +0100

    Delete database.sql

--- database.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE record (
-   user_id int(11) AUTO_INCREMENT PRIMARY KEY not null,
-   type varchar(256) not null,
-   name varchar(256) not null,
-   date TIMESTAMP
-   );
-
-INSERT INTO record (type, name)
-VALUES ('Value 1', 'Ship 1');
